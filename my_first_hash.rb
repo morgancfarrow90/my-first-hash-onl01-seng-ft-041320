c1cb6746c4dbb69c2ed7443a6dcf97520464042b
Morgan Farrow <morgancfarrow@gmail.com>
2020-04-18 22:42:58 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -3,7 +3,7 @@ def my_hash
 end
 
 def shipping_manifest
-  the_manifest = {"whale bone corset" => 5,
+  return the_manifest = {"whale bone corset" => 5,
   "porcelain vase" => 2,
   "oil painting" => 3}
 end
