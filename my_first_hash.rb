e2ed5787e67b4ac8c02458020a0e6da1ecd62987
Morgan Farrow <morgancfarrow@gmail.com>
2020-04-18 22:55:02 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -24,6 +24,7 @@ def adding
                   "oil paintings" => 3
                  }
 shipping_manifest["muskets"] = 2
+shipping_manifest
 shipping_manifest["gun power"] = 4
 shipping_manifest
 end
